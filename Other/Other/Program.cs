1c9ba5a44b7b7d637fa48a286c8afe85373872ab
Kristiyan Dimitrov <Kristiyan_D@outlook.com>
2020-04-27 21:24:44 +0100

    OtherTesting for single testing

---
+++ Other/Other/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating Nullable Types. Use when you need to go arround undeclared variables.
+            int? num1 = null;
+            double? num2 = new Double?();
+            bool? boolean1;
+
+        }
+    }
+}
